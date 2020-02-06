9324fe8b7a737108a7988576685eb7b51e504f96
James Lang <hi@thejameslang.com>
2020-02-05 22:00:56 -0600

    search username and add to firebase on success

--- src/App.js
+++ src/App.js
@@ -4,8 +4,32 @@ import "./App.css";
 import firebase from "./firebase";
 
 function App() {
+  const [searchGitHubUsername, setSearchGitHubUsername] = React.useState("");
+
+  const db = firebase.firestore();
+
+  const onSearch = () => {
+    fetch("https://api.github.com/users/" + searchGitHubUsername, {})
+      .then(response => {
+        if (response.ok) {
+          alert("user found!");
+          response.json().then(data => {
+            console.log(data);
+            db.collection("users").add(data);
+          });
+        } else {
+          alert("user not found!");
+        }
+      })
+      .catch(error => alert("connection issue!"));
+  };
   return (
     <div className="App">
+      <input
+        value={searchGitHubUsername}
+        onChange={e => setSearchGitHubUsername(e.target.value)}
+      />
+      <button onClick={onSearch}>Search</button>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
